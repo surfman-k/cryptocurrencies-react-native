fbf02aff3285b32a76f2e202f19ba18db238a361
Konrad <konrad.kipczak@gmail.com>
2018-03-03 17:30:59 +0000

    added actions for api request

--- src/Actions/FetchCoinData.js
+++ src/Actions/FetchCoinData.js
@@ -1,5 +1,10 @@
 import axios from 'axios';
 import {apiBaseURL} from './../utils/Constants';
+import{
+	FETCHING_COIN_DATA,
+	FETCHING_COIN_DATA_SUCCESS,
+	FETCHING_COIN_DATA_FAIL
+} from './../Utils/ActionTypes';
 
 export default function FetchCoinData() {
 	return dispatch => {
